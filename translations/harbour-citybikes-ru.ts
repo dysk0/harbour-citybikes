4dc6fe79a6008fa48fc301cdf70b1693d6bc99bc
Вячеслав Диконов <sdiconov@mail.ru>
2018-07-02 17:45:17 +0300

    Russian translation
    Transifex does not allow to add this translation (Russian is not offered as a possible language durig a team join request).

---
+++ translations/harbour-citybikes-ru.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.1">
+<context>
+    <name>Categories</name>
+    <message>
+        <location filename="../qml/pages/Categories.qml" line="60"/>
+        <source>Networks</source>
+        <translation>Сети проката</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Categories.qml" line="160"/>
+        <source>Please wait...</source>
+        <translation>Подождите...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Categories.qml" line="160"/>
+        <source>No results found!</source>
+        <translation>Ничего не найдено!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Categories.qml" line="161"/>
+        <source>Loading networks to display</source>
+        <translation>Загружается список прокатов</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="209"/>
+        <source>Remove default location</source>
+        <translation>Забыть текущий город</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="223"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="230"/>
+        <source>Refresh</source>
+        <translation>Обновить</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="237"/>
+        <source>Hide search</source>
+        <translation>Скрыть поле поиска</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="237"/>
+        <source>Show search</source>
+        <translation>Показать поле поиска</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="252"/>
+        <source>No results found!</source>
+        <translation>Ничего не найдено!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="253"/>
+        <source>Please change your inquiry</source>
+        <translation>Измените свой запрос</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="311"/>
+        <source>Flick left to select default location</source>
+        <translation>Перейдите на экран справа для выбора города</translation>
+    </message>
+</context>
+<context>
+    <name>Station</name>
+    <message>
+        <location filename="../qml/pages/Station.qml" line="198"/>
+        <source>Free Bike</source>
+        <translation>Велосипед</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Station.qml" line="198"/>
+        <source>Free Bikes</source>
+        <translation>Велосипедов</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Station.qml" line="205"/>
+        <source>Empty slots</source>
+        <translation>Пустых ячеек</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Station.qml" line="214"/>
+        <source>Far away</source>
+        <translation>Расстояние</translation>
+    </message>
+</context>
+</TS>
